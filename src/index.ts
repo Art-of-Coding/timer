4882f1f5af722cde2d02f04c97360cea483602a1
Michiel van der Velde <michiel@michielvdvelde.nl>
2020-03-10 14:33:38 +0100

    feat: add `removeAllListeners`

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,10 @@ export class Timer {
     return this
   }
 
+  public removeAllListeners () {
+    this.listeners.clear()
+  }
+
   public cancel (): this {
     if (!this.fired) {
       clearTimeout(this.timeout)
